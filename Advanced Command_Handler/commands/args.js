4d6373fcceb9a966fc484396803bd4a9e52e0bf6
(不滅)Shinigami <70421322+JussTorched@users.noreply.github.com>
2021-02-09 12:20:23 -0500

    Delete Advanced Command_Handler directory

--- Advanced Command_Handler/commands/args.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'args',
-    description: 'args example',
-    execute(message, args){
-        const fullMessage = args.join(' ');
-        message.channel.send(fullMessage);
-    }
-
-}
