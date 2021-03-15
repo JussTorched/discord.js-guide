e84d11120c36f6eff26497d214fdd5734429f6b7
weeabooofweebs <ely.btw.bortot@outlook.com>
2021-03-15 03:27:42 -0400

    Fixed to video code

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { token, prefix } = require('./config.json');
+const { token } = require('./config.json');
 
 client.on('ready', () => {
     console.log('Bot is on!');
 });
 
-client.on('message', async message => {
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    if (message.content === preifx + 'img-manip') {
-        if (!args[0]) {
-            return message.channel.send('Please enter an argument.');
-        } else {
-            const Canvas = require('canvas');
-            const fullMessageArgs = args.join(' ');
-            const canvas = Canvas.createCanvas(512, 126);
-            const ctx = canvas.getContext('2d');
-            const background = await Canvas.loadImage('./xbox achievement template.png');
+client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.cache.get('801658010996178944');
+    const embed = new Discord.MessageEmbed()
+        .setTitle('Test')
+        .setDescription('Hello youtube!');
+    channel.send(embed);
+});
 
-            ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-            ctx.strokeStyle = '10px #74037b';
-            ctx.font = '25px sans-serif';
-            ctx.fillStyle = '#d6cec7';
-
-            ctx.fillText(fullMessageArgs, canvas.width / 3.8, canvas.height / 1.4);
-
-            const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'yt-test.jpeg');
-
-            message.channel.send(attachment);
-        }
-    }
+client.on('guildMemberRemove', member => {
+    const channel = member.guild.channels.cache.get('801658010996178944');
+    const embed = new Discord.MessageEmbed()
+        .setTitle('Test')
+        .setDescription('goodbye youtube :(');
+    channel.send(embed);
 });
 
 client.login(token);
