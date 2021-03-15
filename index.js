56d127d49071949c2f7f661f969411defbdd95eb
weeabooofweebs <ely.btw.bortot@outlook.com>
2021-03-15 03:25:55 -0400

    Fix to video code

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { token } = require('./config.json');
+const { token, prefix } = require('./config.json');
 
 client.on('ready', () => {
     console.log('Bot is on!');
+});
 
-    const activities = [
-        'test 1',
-        'test 2',
-        'test 3'
-    ];
-    const randomizer = Math.floor(Math.random() * activities.length);
-    client.user.setActivity(activities[randomizer]);
-    setInterval(() => {
-        client.user.setActivity(activities[randomizer]);
-    }, 30000);
+client.on('message', async message => {
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    if (message.content === preifx + 'img-manip') {
+        if (!args[0]) {
+            return message.channel.send('Please enter an argument.');
+        } else {
+            const Canvas = require('canvas');
+            const fullMessageArgs = args.join(' ');
+            const canvas = Canvas.createCanvas(512, 126);
+            const ctx = canvas.getContext('2d');
+            const background = await Canvas.loadImage('./xbox achievement template.png');
+
+            ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+            ctx.strokeStyle = '10px #74037b';
+            ctx.font = '25px sans-serif';
+            ctx.fillStyle = '#d6cec7';
+
+            ctx.fillText(fullMessageArgs, canvas.width / 3.8, canvas.height / 1.4);
+
+            const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'yt-test.jpeg');
+
+            message.channel.send(attachment);
+        }
+    }
 });
 
 client.login(token);
