f362bb1a5a7da21af0365f2d16d8fc33d14893c2
weeabooofweebs <ely.btw.bortot@outlook.com>
2021-03-15 03:13:14 -0400

    Fixed to video code

--- index.js
+++ index.js
@@ -6,4 +6,23 @@ client.on('ready', () => {
     console.log('Bot is on!');
 });
 
+client.on('message', message => {
+    if (message.content.toLowerCase() === 'embed') {
+        const embed = new Discord.MessageEmbed()
+            .setTitle('test')
+            .setDescription('description here [link](link)')
+            .setColor(255, 255, 255)
+            .setThumbnail('media-link')
+            .setImage('media-link')
+            .addFields(
+                { name: 'field name', value: 'field value', inline: true }
+            )
+            .setAuthor('WeebCodes', 'media-link', 'link')
+            .setFooter('footer', 'media-link')
+            .setTimestamp()
+
+        message.channel.send(embed);
+    }
+});
+
 client.login(token);
